13a77b0081abfeb48b7adb4bfa55b61e035d4974
avasconcelos-tech <a.vasconcelos@edu.senai.br>
2026-09-05 02:01:03 +0000

    impletação do código do sprites.js

---
+++ src/sprites.js
@@ -0,0 +1,25 @@
+// Carrega as artes SVG originais do jogo como Image() prontas para o canvas.
+
+const paths = {
+  hero: '/assets/crash2.gif',
+  fruit: '/assets/fruit.svg',
+  crate: '/assets/crate.svg',
+  boulder: '/assets/boulder.svg',
+  background: '/assets/fundo.jpeg'
+};
+
+function loadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+    img.src = src;
+  });
+}
+
+export async function loadSprites() {
+  const entries = await Promise.all(
+    Object.entries(paths).map(async ([key, src]) => [key, await loadImage(src)])
+  );
+  return Object.fromEntries(entries);
+}
